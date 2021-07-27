07d17733095c3a3e2a18385e4f78da9cfb904b48
raph <raph@e-frogg.com>
2021-07-27 12:39:37 +0200

    sdkModelSerializer can now be used without using the factory. It works just like the Serializer

--- src/Apistarter/Sdk/Serializer/SdkModelSerializer.php
+++ src/Apistarter/Sdk/Serializer/SdkModelSerializer.php
@@ -9,14 +9,27 @@
 namespace Apistarter\Sdk\Serializer;
 
 
+use Symfony\Component\Serializer\Encoder\ContextAwareDecoderInterface;
+use Symfony\Component\Serializer\Encoder\ContextAwareEncoderInterface;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
+use Symfony\Component\Serializer\Normalizer\ContextAwareDenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
 use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
 
-class SdkModelSerializer
+class SdkModelSerializer implements SerializerInterface, ContextAwareNormalizerInterface, ContextAwareDenormalizerInterface, ContextAwareEncoderInterface,
+                                    ContextAwareDecoderInterface
 {
+    private Serializer $serializer;
+
+    public function __construct()
+    {
+        $this->serializer = self::factory();
+    }
+
     public static function factory(): Serializer
     {
         $normalizers = [
@@ -27,6 +40,61 @@ class SdkModelSerializer
         ];
 
         return new Serializer($normalizers, [new XmlEncoder(), new JsonEncoder()]);
-
     }
+
+    public function __call($name, $arguments)
+    {
+        return call_user_func_array([$this->serializer, $name], $arguments);
+    }
+
+    public function supportsDecoding(string $format, array $context = [])
+    {
+        return $this->serializer->supportsDecoding(...func_get_args());
+    }
+
+    public function supportsDenormalization($data, string $type, string $format = null, array $context = [])
+    {
+        return $this->serializer->supportsDenormalization(...func_get_args());
+    }
+
+    public function supportsEncoding(string $format, array $context = [])
+    {
+        return $this->serializer->supportsEncoding(...func_get_args());
+    }
+
+    public function supportsNormalization($data, string $format = null, array $context = [])
+    {
+        return $this->serializer->supportsNormalization(...func_get_args());
+    }
+
+    public function decode(string $data, string $format, array $context = [])
+    {
+        return $this->serializer->decode(...func_get_args());
+    }
+
+    public function denormalize($data, string $type, string $format = null, array $context = [])
+    {
+        return $this->serializer->denormalize(...func_get_args());
+    }
+
+    public function encode($data, string $format, array $context = [])
+    {
+        return $this->serializer->encode(...func_get_args());
+    }
+
+    public function normalize($object, string $format = null, array $context = [])
+    {
+        return $this->serializer->normalize(...func_get_args());
+    }
+
+    public function serialize($data, string $format, array $context = [])
+    {
+        return $this->serializer->serialize(...func_get_args());
+    }
+
+    public function deserialize($data, string $type, string $format, array $context = [])
+    {
+        return $this->serializer->deserialize(...func_get_args());
+    }
+
 }
